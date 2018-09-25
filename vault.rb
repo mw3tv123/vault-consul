9ddb0444f1c2dc3f98896b3a19d2925854f824da
Hung Tran <tqhung1@tma.com.vn>
2018-09-25 17:29:54 +0700

    Update vault.rb

--- vault.rb
+++ vault.rb
@@ -4,19 +4,22 @@ apt_update
 # Get the 'unzip'package
 package 'unzip'
 
-# Fetching Consul
+# Fetching Vault
 remote_file 'vault_0.11.1_linux_amd64.zip' do
   source 'https://releases.hashicorp.com/vault/0.11.1/vault_0.11.1_linux_amd64.zip'
   show_progress true
   checksum 'eb8d2461d0ca249c1f91005f878795998bdeafccfde0b9bae82343541ce65996'
 end
 
-# Extract Consul
+directory '$HOME/vault' do
+  # action :create
+end
+
+# Extract Vault
 bash 'extract_module' do
   code <<-EOH
-    mkdir -p vault
     echo "Unziping Vault ... "
-    unzip vault_0.11.1_linux_amd64.zip -d vault/
+    unzip -fo vault_0.11.1_linux_amd64.zip -d vault/
     EOH
   not_if { ::File.exist?('~/vault/vault') }
 end
