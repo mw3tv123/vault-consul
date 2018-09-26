d432c955dba25170d379c3443ca410eb29a74352
root <root@sv2.devops.tma.com.vn>
2018-09-26 03:23:02 +0000

    update vault.rb

--- vault.rb
+++ vault.rb
@@ -18,7 +18,7 @@ end
 
 # Extract Vault
 execute 'extract_module' do
-  command 'unzip -fo vault_0.11.1_linux_amd64.zip -d /opt/vault/'
+  command 'unzip -o vault_0.11.1_linux_amd64.zip -d /opt/vault/'
   live_stream true
   not_if { ::File.exist?('/opt/vault/vault') }
 end
