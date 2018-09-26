e53d6b0687e7fde4a4d540a7296bc6d9051eacb2
Hung Tran <tqhung1@tma.com.vn>
2018-09-26 15:56:04 +0700

    Vast update :V

--- cookbooks/install_consul/recipes/default.rb
+++ cookbooks/install_consul/recipes/default.rb
@@ -5,15 +5,17 @@ apt_update
 package 'unzip'
 
 # Fetching Consul
-remote_file 'downoad_consul_binary' do
+remote_file 'download_consul_binary' do
   source 'https://releases.hashicorp.com/consul/1.2.3/consul_1.2.3_linux_amd64.zip'
   path '/tmp/consul_1.2.3_linux_amd64.zip'
   show_progress true
   checksum 'f97996296ef3905c38c504b35035fb731d3cfd9cad129c9523402a4229c709c5'
+  not_if { ::File.exist?('/tmp/consul_1.2.3_linux_amd64.zip') }
 end
 
 directory '/opt/consul' do
   # action :create
+  not_if { ::Dir.exist?('/opt/consul') }
 end
 
 # Extract Consul
@@ -27,10 +29,12 @@ link 'symlink_vault' do
   link_type :symbolic
   target_file '/usr/bin/consul'
   to '/opt/consul'
+  not_if { ::File.symlink?('/usr/bin/consul') }
 end
 
 template '/etc/systemd/system/consul.service' do
   source 'consul.service.erb'
+  not_if { ::File.exist?('/etc/systemd/system/consul.service') }
 end
 
 service 'consul' do
--- cookbooks/install_vault/recipes/default.rb
+++ cookbooks/install_vault/recipes/default.rb
@@ -10,10 +10,19 @@ remote_file 'download_vault_binary' do
   path '/tmp/vault_0.11.1_linux_amd64.zip'
   show_progress true
   checksum 'eb8d2461d0ca249c1f91005f878795998bdeafccfde0b9bae82343541ce65996'
+  not_if { ::File.exist?('/tmp/vault_0.11.1_linux_amd64.zip') }
 end
 
 directory '/opt/vault' do
   # action :create
+  not_if { ::Dir.exist?('/opt/vault') }
+end
+
+remote_file 'copy_config' do
+  source 'file:///$HOME/vault_consul/config.hcl'
+  path '/opt/vault/config.hcl'
+  show_progress true
+  not_if { ::File.exist?('/opt/vault/config.hcl') }
 end
 
 # Extract Vault
@@ -27,4 +36,14 @@ link 'symlink_vault' do
   link_type :symbolic
   target_file '/usr/bin/vault'
   to '/opt/vault'
+  not_if { ::File.symlink?('/usr/bin/vault') }
+end
+
+template '/etc/systemd/system/vault.service' do
+  source 'vault.service.erb'
+  not_if { ::File.exist?('/etc/systemd/system/vault.service') }
+end
+
+service 'vault' do
+  action [:enable, :start]
 end
